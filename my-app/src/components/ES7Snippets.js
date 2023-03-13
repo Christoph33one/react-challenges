0a7e5c4bd3c8f7f95bcda96b34d6ab87cdbeeacc
Christoph33one <cjbrowning23@hotmail.com>
2023-03-13 18:39:05 +0000

    Handling events challege create a login button to conditionaly render a form and sumbit buton. Also using the nested components methid to to do the same but with a child component

---
+++ my-app/src/components/ES7Snippets.js
@@ -0,0 +1,63 @@
+// rfce Creates react functional component
+
+// import React from 'react'
+// function ES7Snippets() {
+//   return (
+//     <div>ES7Snippets</div>
+//   )
+// }
+
+// export default ES7Snippets
+
+
+
+// rafce Creates a react arrow functional component
+
+// import React from 'react'
+
+// const ES7Snippets = () => {
+//   return (
+//     <div>ES7Snippets</div>
+//   )
+// }
+
+// export default ES7Snippets
+
+
+
+// rce Creates a react class component
+
+// import React, { Component } from 'react'
+
+// export class ES7Snippets extends Component {
+//   render() {
+//     return (
+//       <div>ES7Snippets</div>
+//     )
+//   }
+// }
+
+// export default ES7Snippets
+
+
+
+// rconst Adds a default constructor for a class that contains props as arguments
+
+// constructor(props) {
+//   super(props)
+
+//   this.state = {
+//      first
+//   }
+// }
+
+
+// clg creates console.lg 
+
+// console.log(first)
+
+
+
+// clo creats console.log
+
+// console.log('first', first)
---
+++ my-app/src/components/NavBarForm.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react'
+import css from "./css/NavBarForm.module.css";
+import NavBarChild from './NavBarChild';
+
+
+
+class NavBarForm extends Component {
+    constructor(props) {
+        super(props)
+        
+        this.state = {
+            isLoggedIn: true,
+        }
+    }
+    handleButtonClick = () => {
+        this.setState((prevState) => ({
+            isLoggedIn: prevState.isLoggedIn ? false: true
+        }),  () => console.log(this.state.isLoggedIn))
+    }
+
+
+
+// {this.state.isLoggedIn}, conditionaly renders LogedIn to Submit when the user clicks!
+  render() {
+    return (
+        <div className= {css.NavBar}>
+            <h1>My Gallery</h1>
+            
+                {/* { this.state.isLoggedIn ? 
+                    <button onClick={() => this.handleButtonClick()}>LogIn</button>
+
+                :
+
+                    <form>
+                        <label>User name:</label>
+                        <input placeholder='username' id='username' />
+
+                        <label>password:</label>
+                        <input placeholder='password' id='password' />
+                        <button onClick={() => this.handleButtonClick()}>Submit</button>
+                    </form>
+                } */}
+
+            <NavBarChild
+            isLoggedIn={this.state.isLoggedIn}
+            handleClick={this.handleButtonClick}
+            />
+        </div>
+        // NESTED COMPONENTS METHOD
+        // Import the child component (NavBarchild)
+        // Here we used nested compoents method by passing the above code to a child file
+        // Then with the method above, passed the isLoggedIn and handleClick as props to the child component.
+        //
+
+    )
+  }
+}
+
+export default NavBarForm
