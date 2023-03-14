a9e41519cbefdf98f641ead4cb17b84714be6b0f
Christoph33one <cjbrowning23@hotmail.com>
2023-03-14 15:29:29 +0000

    using the map method to render all items from an array. Then using nested componts method, to render the arra items as props in a child component (postitem.js)

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,17 @@
 import css from "./App.module.css";
 // import NavBarSimple from "./components/NavBarSimple";
-import Sidebar from "./components/Sidebar";
-import NavBarForm from "./components/NavBarForm";
+// import Sidebar from "./components/Sidebar";
+// import NavBarForm from "./components/NavBarForm";
+import Content from "./components/Content";
 
 function App() {
   return (
     <div className={css.App}>
       {/* Add your components here! */}
       {/* <NavBarSimple /> */}
-      <Sidebar />
-      <NavBarForm />
+      {/* // <Sidebar />
+      // <NavBarForm /> */}
+      <Content />
     </div>
   );
 }
