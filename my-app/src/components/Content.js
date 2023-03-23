ef92801af2b217ff1adc1a34ac4ca53108d5b2fe
Christoph33one <cjbrowning23@hotmail.com>
2023-03-23 11:07:47 +0000

    added loading effect with css, and added componentDidMount with time r to run for 2 seconds, then display images

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -2,11 +2,24 @@ import React, { Component } from 'react'
 import css from "./css/Content.module.css";
 import {savedPosts} from "../posts.json";
 import PostItem from './PostItem';
+import Loader from './Loader';
 
 export class Content extends Component {
     constructor(props) {
         super(props)
+        this.state = {
+          isLoaded: false
+        }
     }
+
+    componentDidMount() {
+      setTimeout(()=>{
+          this.setState({
+              isLoaded: true,
+          })
+      }, 2000)
+  }
+
   render() {
     return (
       <div className={css.Content}>
@@ -15,18 +28,20 @@ export class Content extends Component {
         </div>
 
         <div className={css.SearchResults}>
+            {
+                this.state.isLoaded ? <PostItem savedPosts={savedPosts} />:<Loader />
+            }
+        </div>
 
-            {/* {savedPosts.map((post) => {
+            {savedPosts.map((post) => {
                 return <div className={css.SearchResults} key={post.title}>
                    <p>{post.title}</p>
                    <p>post.name</p>
                    <img src={post.image} alt="random"/>
                    <p>{post.description}</p>
                     </div>
-            })} */}
-            <PostItem savedPosts={savedPosts} />
+            })}
         </div>
-      </div>
     )
   }
 }
