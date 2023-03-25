1314fdd2a71a1ab8b7e5dc034fd33588eec5a670
Christoph33one <cjbrowning23@hotmail.com>
2023-03-25 14:36:25 +0000

    challege for useState and useEffect

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,9 @@
 import css from "./App.module.css";
-import NavBarSimple from "./components/NavBarSimple";
-import Sidebar from "./components/Sidebar";
-import NavBarForm from "./components/NavBarForm";
-import Content from "./components/Content";
+// import NavBarSimple from "./components/NavBarSimple";
+// import Sidebar from "./components/Sidebar";
+// import NavBarForm from "./components/NavBarForm";
+// import Content from "./components/Content";
+import ContentHooks from "./components/ContentHooks";
 
 
 
@@ -10,10 +11,11 @@ function App() {
   return (
     <div className={css.App}>
       Add your components here!
-      <NavBarSimple />
-      <Sidebar />
-      <NavBarForm />
-      <Content />
+      {/* <NavBarSimple /> */}
+      {/* <Sidebar /> */}
+      {/* <NavBarForm /> */}
+      {/* <Content /> */}
+      <ContentHooks />
 
     </div>
   );
