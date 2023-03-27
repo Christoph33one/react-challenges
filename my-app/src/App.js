38b3b3b6b2b49f09d7df2335062204bdf631f1b1
Christoph33one <cjbrowning23@hotmail.com>
2023-03-27 13:28:54 +0000

    refractored contentAPI.js with hooks in a new file - ContentAPIHooks.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,9 +4,8 @@ import Sidebar from "./components/Sidebar";
 import NavBarForm from "./components/NavBarForm";
 // import Content from "./components/Content";
 // import ContentHooks from "./components/ContentHooks";
-import ContentAPI from "./components/ContentAPI";
-
-
+import ContentAPI from "./components/ContentAPIHooks";
+import ContentAPIHooks from "./components/ContentAPIHooks";
 
 
 function App() {
@@ -18,7 +17,8 @@ function App() {
       <NavBarForm />
       {/* <Content /> */}
       {/* <ContentHooks /> */}
-      <ContentAPI />
+      {/* <ContentAPI /> */}
+      <ContentAPIHooks />
     </div>
   );
 }
